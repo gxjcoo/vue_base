f9f40dd77040520de660f1c5a15b5a50fa0ae093
gxjcoo <gxjcoo@gmail.com>
2020-03-04 23:52:24 +0800

    upd

--- createblognginx.sh
+++ createblognginx.sh
@@ -1,7 +1,7 @@
 sudo docker stop blognginx || true \
  && sudo docker rm blognginx || true \
  && pwd \
- && sudo cd /root/docker/jenkins_home/workspace/vuedemo  \
+ && sudo cd /root/docker/jenkins_home/workspace/blog  \
  && pwd \
  && sudo docker  build ./  -t blognginx   \
- && sudo docker run -d -p 80:80 --name blognginx -v /root/docker/jenkins_home/workspace/vuedemo/dist:/usr/share/nginx/html -v /root/docker/jenkins_home/workspace/vuedemo/nginx.conf:/etc/nginx/nginx.conf blognginx
+ && sudo docker run -d -p 80:80 --name blognginx -v /root/docker/jenkins_home/workspace/blog/dist:/usr/share/nginx/html -v /root/docker/jenkins_home/workspace/blog/nginx.conf:/etc/nginx/nginx.conf blognginx
